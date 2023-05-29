64034d6b3c9b99f6fcc7b507198e1e8d76743a26
Viktoria Rosovska <torimeister@gmail.com>
2023-05-29 18:24:23 +0300

    add componemts and news-service class

--- src/js/news-service.js
+++ src/js/news-service.js
@@ -1,41 +1,39 @@
-const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
+const API_KEY = 'a82dcb882c624586a39b06ebd5abc436';
+
 const BASE_URL = 'https://newsapi.org/v2';
+
 const options = {
-  headers: {
-    Authorization: API_KEY,
-  },
+     headers: {
+         Authorization: API_KEY,
+     }
 };
-
+ 
 export default class NewsApiService {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+     } 
+    fetchArticles() {    
+        const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&sortBy=poularity&page=${this.page}&pageSize=6`
+        
+        return fetch(url, options)
+            .then(response => response.json())
+            .then(({articles}) => {
+                this.incrementPage();
+                return articles;
+            });
+    }
+    get query() {
+        return this.searchQuery;
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    }
 
-  fetchArticles() {
-    const url = `${BASE_URL}/everything?q=${this.searchQuery}&language=en&pageSize=5&page=${this.page}`;
-
-    return fetch(url, options)
-      .then(response => response.json())
-      .then(({ articles }) => {
-        this.incrementPage();
-        return articles;
-      });
-  }
-
-  incrementPage() {
-    this.page += 1;
-  }
-
-  resetPage() {
-    this.page = 1;
-  }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
-}
